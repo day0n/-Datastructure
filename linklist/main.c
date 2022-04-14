8a18207a82d80bdafe40b05d77542f5854750681
day0n <mrteniu@gmail.com>
2022-04-14 21:13:26 +0800

    test

--- linklist/main.c
+++ linklist/main.c
@@ -5,7 +5,7 @@ int main()
     temp = (node *)malloc(sizeof(node));
     L = temp;
     init(L, 5);
-    printI(L, 0);
+    printI(L, 10);
     insert(L, 2, 100);
     printf("删除元素的值是%d\n",delete(L,3));
     printlength(L);
